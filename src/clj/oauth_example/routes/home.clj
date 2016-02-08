8249aa78377443c6e8dc7fd52a75074e434ebec5
Yogthos <dmitri.sotnikov@gmail.com>
2016-02-08 09:39:18 -0500

    improved example

--- src/clj/oauth_example/routes/home.clj
+++ src/clj/oauth_example/routes/home.clj
@@ -3,11 +3,11 @@
             [compojure.core :refer [defroutes GET]]
             [ring.util.http-response :refer [ok found]]
             [clojure.java.io :as io]
-            [oauth-example.routes.twitter-oauth :as tw]))
+            [oauth-example.routes.twitter-oauth :as tw]
+            [clojure.tools.logging :as log]))
 
-(defn home-page [& [opts]]
-  (layout/render
-    "home.html" opts))
+(defn home-page [opts]
+  (layout/render "home.html" opts))
 
 (defn about-page []
   (layout/render "about.html"))
@@ -22,15 +22,20 @@
 
 (defn twitter-callback
   "Handles the callback from Twitter."
-  [request_token {session :session}]
+  [request_token {:keys [session]}]
+  ; oauth request was denied by user
   (if (:denied request_token)
-    (found "/") ; oauth request was denied by user
+    (-> (found "/")
+        (assoc :flash {:denied true}))
     ; fetch the request token and do anything else you wanna do if not denied.
-    (home-page {:user (tw/fetch-access-token request_token)})))
+    (let [{:keys [user_id screen_name]} (tw/fetch-access-token request_token)]
+      (log/info "successfully authenticated as" user_id screen_name)
+      (-> (found "/")
+          (assoc :session
+            (assoc session :user-id user_id :screen-name screen_name))))))
 
 (defroutes home-routes
-  (GET "/" [] (home-page))
-  (GET "/denied" [] (home-page {:denied true}))
+  (GET "/" {:keys [flash session]} (home-page (or flash session)))
   (GET "/about" [] (about-page))
   (GET "/oauth/twitter-init" req (twitter-init req))
   (GET "/oauth/twitter-callback" [& req_token :as req] (twitter-callback req_token req)))
